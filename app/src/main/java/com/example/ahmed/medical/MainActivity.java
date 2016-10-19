a50c6a97f2e629f31739e41c2dbef4af880556b3
emanzelekha <emanzelekha@gmail.com>
2016-10-19 03:06:19 -0700

    emancommit2

--- app/src/main/java/com/example/ahmed/medical/MainActivity.java
+++ app/src/main/java/com/example/ahmed/medical/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.ahmed.medical;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import static com.example.ahmed.medical.R.attr.height;
+import static com.example.ahmed.medical.R.id.top;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView t1=(TextView)findViewById(R.id.text1);
+
+        Display display =((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int width = display.getWidth();
+        int height=display.getHeight();
+   RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(width/2, width/2);
+        t1.setLayoutParams(layoutParams);
+
+
     }
 }
