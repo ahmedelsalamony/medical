6781ad4d568be25320ee27e6e504898ca98b5441
emanzelekha <emanzelekha@gmail.com>
2016-10-23 20:36:32 -0700

    emancommit6

---
+++ app/src/main/java/com/example/ahmed/medical/MangmentAndDepartment/MangmentAndDepartment.java
@@ -0,0 +1,15 @@
+package com.example.ahmed.medical.MangmentAndDepartment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ahmed.medical.R;
+
+
+public class MangmentAndDepartment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mangment_and_department);
+    }
+}
